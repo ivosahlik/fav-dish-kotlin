1ed7181501a0ffae467f8283a629451992f481db
Ivo Vošahlík, External <ivosahlik@seznam.cz>
2024-07-23 12:03:24 +0200

    init commit

---
+++ app/src/main/java/cz/ivosahlik/fav_dish/MainActivity.kt
@@ -0,0 +1,13 @@
+package cz.ivosahlik.fav_dish
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
