bc897a4b8a8fe576ad0164f9614e9ceb65e4e874
Ivo Vošahlík, External <ivosahlik@seznam.cz>
2024-07-24 00:42:48 +0200

    refactoring, added custom image selection dialog

--- app/src/main/java/cz/ivosahlik/fav_dish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/cz/ivosahlik/fav_dish/view/activities/AddUpdateDishActivity.kt
@@ -1,11 +1,13 @@
 package cz.ivosahlik.fav_dish.view.activities
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import cz.ivosahlik.fav_dish.R
 import cz.ivosahlik.fav_dish.databinding.ActivityAddUpdateDishBinding
+import cz.ivosahlik.fav_dish.databinding.DialogCustomImageSelectionBinding
 
 class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var mBinding: ActivityAddUpdateDishBinding
@@ -30,10 +32,37 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
         if (v != null) {
             when (v.id) {
                 R.id.iv_add_dish_image -> {
-                    Toast.makeText(this@AddUpdateDishActivity, "You have clicked the ImageView", Toast.LENGTH_SHORT).show()
+                    //Toast.makeText(this@AddUpdateDishActivity, "You have clicked the ImageView", Toast.LENGTH_SHORT).show()
+                    customImageSelectionDialog()
                     return
                 }
             }
         }
+    }
+
+    private fun customImageSelectionDialog() {
+        val dialog = Dialog(this@AddUpdateDishActivity)
+        val binding: DialogCustomImageSelectionBinding = DialogCustomImageSelectionBinding.inflate(layoutInflater)
+        dialog.setContentView(binding.root)
+
+        binding.tvCamera.setOnClickListener {
+            Toast.makeText(
+                this@AddUpdateDishActivity,
+                "Camera!",
+                Toast.LENGTH_SHORT
+            ).show()
+            dialog.dismiss()
+        }
+
+        binding.tvGallery.setOnClickListener {
+            Toast.makeText(
+                this@AddUpdateDishActivity,
+                "Gallery!",
+                Toast.LENGTH_SHORT
+            ).show()
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 }
