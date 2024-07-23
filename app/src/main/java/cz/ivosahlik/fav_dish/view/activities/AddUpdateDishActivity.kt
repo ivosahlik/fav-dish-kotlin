85fa87f7a1e1a522a199f8b00a838a92276f0809
Ivo Vošahlík, External <ivosahlik@seznam.cz>
2024-07-23 23:36:13 +0200

    added AddUpdateDishActivity

--- app/src/main/java/cz/ivosahlik/fav_dish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/cz/ivosahlik/fav_dish/view/activities/AddUpdateDishActivity.kt
@@ -1,12 +1,28 @@
 package cz.ivosahlik.fav_dish.view.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import cz.ivosahlik.fav_dish.R
+import androidx.appcompat.app.AppCompatActivity
+import cz.ivosahlik.fav_dish.databinding.ActivityAddUpdateDishBinding
 
 class AddUpdateDishActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityAddUpdateDishBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+
+        setContentView(mBinding.root)
+
+        setupActionBar()
     }
+
+    /**
+     * A function for ActionBar setup.
+     */
+    private fun setupActionBar() {
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+
 }
